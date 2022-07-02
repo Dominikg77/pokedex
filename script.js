9610259ca27287c8557f9d2b406b675c0327e369
Dominik <dominik.graf2001@gmail.com>
2022-07-02 11:19:33 +0200

    new desing

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let currentPokoemon
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 let allPokemon = [];
+let numberOfPokemon = 30;
 
 
 async function loadPokemon() { // async benötigetes da es await drin hat
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < numberOfPokemon; i++) {
         const pokemon_url = url + (i + 1);
         let response = await fetch(pokemon_url); // fetch() funktion zum auf API zugreifen / await ist ein warte befehl
         currentPokemon = await response.json(); // wird zu einem JSON /
@@ -28,6 +29,7 @@ function generateMainHtml(i, name, types, img) {
     return `
      <div onclick="renderDialog(${i})" class="container-main" id="pokex-container-${i}">
      <div class="main-info-container">
+
      <h1 id="pokemon-name">${name}</h1>
      <div class="second-home-info">
          <p id="home-info"> ${types} </p>
@@ -44,6 +46,8 @@ function renderDialog(i) {
     let name = allPokemon[i][`name`];
     let types = allPokemon[i][`types`][0][`type`][`name`];
     let img = allPokemon[i][`sprites`][`other`][`dream_world`][`front_default`];
+    let height = allPokemon[i][`height`]; // zugreifen auf den namen 
+    let weight = allPokemon[i][`weight`]; // zugreifen auf den namen 
     document.getElementById(`dialog`).classList.remove(`d-none`);
     let mainContainer = document.getElementById('dialog');
     mainContainer.innerHTML = `
@@ -51,12 +55,33 @@ function renderDialog(i) {
 <div class="container-spez-info-width" >
 <div class="dialog-info" >
 <h1>${name}</h1>
-<div class="dialog-types" >
-<p>${types}</p>
-</div>
 </div>
 <div id="second-info-dialog" >
+<div class="image" >
+<img src="img/left.png" id="left-Image">
 <img src="${img}" id="pokomon-Image">
+<img src="img/right.png" id="right-Image">
+</div>
+<div class="spez-over-info-container" >
+<div class="spez-over-info" >
+<span class="margin-bottom-spez" > <b> Typ </b> </span>
+<span>${types}</span>
+</div>
+
+
+<div class="spez-over-info" >
+<span class="margin-bottom-spez" > <b> Height</b></span>
+<span>${height} ft</span>
+</div>
+
+
+<div class="spez-over-info" >
+<span class="margin-bottom-spez" > <b> Weight</b></span>
+<span>${weight} lbs</span>
+</div>
+
+
+</div>
 
 </div>
 </div>
@@ -70,9 +95,9 @@ function renderDialog(i) {
         mainContainer.innerHTML += `
         <div class="spez-info-container">
     <div class="spez-info" >
-    <div class="attack" > ${attack}: </div>
+    <div class="attack" > <b> ${attack}</b> </div>
     <div class="nr-diagram" > 
-    <div class="nr-digram-bar" style="width: ${nrBar}%; background-color: red"> ${nr} </div>
+    <div class="nr-digram-bar" style="width: ${nrBar}%; background-color: green"> ${nr} </div>
     </div>
     </div>
     `;
@@ -85,6 +110,11 @@ function closeDialog() {
 }
 
 
+function newNumber() {
+    numberOfPokemon = document.getElementById(`number-pokemons`).value;
+    loadPokemon();
+    document.getElementById(`number-pokemons`).value = ``;
+}
 
 
 // Attack Infos 
